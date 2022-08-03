afa994df34a66c81150319ae6cb72210180f40b1
Ajay <ajay.bhan@indianexpress.com>
2022-08-03 13:19:12 +0530

    adding the rest

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String message =  "The IE" + " online";
+        System.out.println(message.replace("e", "#" ));
+    }
+}
+
